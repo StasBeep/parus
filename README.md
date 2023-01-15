b39ce6b614370a58f88b5ddf6ab7cff3d0ba6f7a
StasBeep <stas-stas.stanislav@mail.ru>
2023-01-15 18:43:05 +0300

    Начало пути программирования для первого курса

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # parus
-
-Cсылка на макет
-https://www.figma.com/file/pS0KnmQeYHoKPXXrocNBB8/Product?node-id=0%3A1
--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
 "use strict";
-
-let operand1 = +prompt("Введите число:");
-let operand2 = +prompt("Введите второе число:");
-let operand3 = +prompt("Введите второе число:");
-
-console.log(operand1, operand2, operand3);
-
-// Преположение
-let minEl = operand1;
-
-if(minEl > operand2) {
-    minEl = operand2;
-}
-
-if(minEl > operand3) {
-    minEl = operand3;
-}
-
-alert(`Минимальное число: ${minEl} дорогой пользователь`);
+console.log('Привет');
