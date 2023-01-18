35eedf1ad41dee19cff922551fd3ba2f44921bd7
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2023-01-18 19:59:59 +0300

    код для первой лабораторной работы (пример)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Привет');
+let x = +prompt("Введите x:");
+let y = Number(prompt("Введите y:"));
+
+console.log(x+y);
+
+let t = Math.cos(Math.PI / 7) * Math.pow(Math.sin(x-8*y), 2) / (2,7*(x-Math.PI));
+
+alert("Результа t: " + t);
+
+console.log(t);
