fc68822d10946e9ecf7c487fbdb3cc9041f8afd8
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2023-02-06 11:48:09 +0300

    пояснения для второй лабораторной работы

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ let result = storona(R, n);
 
 console.log(result);
 
+// Функция (метод) входные параметры: R, n
  function storona(R, n) {
     let a = 2 * R * Math.sin(180/n);
     let p = n * a;
+    // return - возврат значения p в result (строка 6)
     return p;
  }
