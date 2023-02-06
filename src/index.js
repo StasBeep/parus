75a71ca431e8f54d3094660306f7e34053554ad2
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2023-02-06 08:57:33 +0300

    Пояснения работы (первая)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 "use strict";
+let result = document.querySelector("#first");
 let x = +prompt("Введите x:");
 let y = Number(prompt("Введите y:"));
 
+// Вывод суммы x+y
 console.log(x+y);
 
 let t = Math.cos(Math.PI / 7) * Math.pow(Math.sin(x-8*y), 2) / (2,7*(x-Math.PI));
 
-alert("Результа t: " + t);
+result.innerText = t;
 
 console.log(t);
