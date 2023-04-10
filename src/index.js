94858f4f9ef40882d331c354a0363dbecaf606b2
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2023-04-10 09:29:30 +0300

    4 лабораторная работа

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 "use strict";
 
-// Первый вид цикла
-for(let i = 0; i < 100; i++) {
-   // i++ => i = i + 1;
-   console.log('Привет, мир!');
+let y;
+// i+= 0.3 => i = i + 0.3;
+for (let x = -3; x <= 3; x += 0.3) {
+   if (x >= -2 && x <= 2) {
+      y = Math.pow(x, 2);
+   } else if (x > 2) {
+      y = 0;
+   } else if (x < -2) {
+      y = 4 * Math.cos(x);
+   }
+
+   console.log(y);
 }
-
-
-// Второй вид цикла
-let i = 0;
-
-while(i < 100) {
-   console.log('Привет, мир!');
-   i++;
-}
-
-// Третий вид цикла
-
-i = 100;
-
-do {
-   console.log('Привет, мир!');
-   i++;
-} while (i < 100)
