406a5fc5f4a68d5e095c57b2ecea755292ddd257
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2023-04-10 09:19:42 +0300

    теория по циклам

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
 "use strict";
 
-// Входные переменные
-let c = +prompt("Введите c:");
-let y = +prompt("Введите y:");
-let x = +prompt("Введите x:");
-let z = +prompt("Введите z:");
-
-// Выходные переменные
-let result = Variant31(c, y, x, z);
-
-// Функция условий
-function Variant31(C, Y, X, Z) {
-   // Переменная для хранения результата
-   let p;
-   // Если с логическим "И"
-   if(Y*Z > 0 && X > 0){
-      let minEl2 = Math.min(C+Y, Math.sqrt(x));
-      p = Math.max(minEl2, Y, X + Z);
-      // Если с уловием и логическим "И"
-   } else if(Y*Z < 0 && X > 0) {
-      p = Math.min(Math.sin(X), Math.pow(Z, 3));
-      // В противном случае
-   } else {
-      p = 1;
-   }
-   // Возврат результата
-   return p;
+// Первый вид цикла
+for(let i = 0; i < 100; i++) {
+   // i++ => i = i + 1;
+   console.log('Привет, мир!');
 }
-// Вывод в консоль
-console.log(result);
+
+
+// Второй вид цикла
+let i = 0;
+
+while(i < 100) {
+   console.log('Привет, мир!');
+   i++;
+}
+
+// Третий вид цикла
+
+i = 100;
+
+do {
+   console.log('Привет, мир!');
+   i++;
+} while (i < 100)
